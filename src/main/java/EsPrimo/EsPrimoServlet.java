cf5de2b38c69efab0397408d5b2dd0a98fe02260
treicko <leo_mejia_p@hotmail.com>
2013-12-11 19:03:39 -0400

    agregando servlet jetty

---
+++ src/main/java/EsPrimo/EsPrimoServlet.java
@@ -0,0 +1,28 @@
+package EsPrimo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class EsPrimoServlet extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		java.io.PrintWriter out = response.getWriter();
+		
+		EsPrimo primo = new EsPrimo();
+		
+		out.println("<h1> Hello word desde Es Primo servlet </h1>");
+		String num = request.getParameter("numero");
+		boolean resp = primo.EsNumeroPrimo(Integer.parseInt(num));
+		
+		out.println("<br><br><h1> El numero: "+resp+" es primo: "+resp+"</h1>");
+		out.close();
+	}
+}
